3e00b8afa0fe5761cacb3394be35f5662b0c3500
yangmr <759168442@qq.com>
2024-03-22 15:31:23 +0800

    update

--- src/main.js
+++ src/main.js
@@ -5,9 +5,35 @@ import './styles/index.scss'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import { qiankunWindow, renderWithQiankun } from 'vite-plugin-qiankun/dist/helper'
 
-app.use(createPinia())
-app.use(router)
+renderWithQiankun({
+  // 挂载时
+  mount(props) {
+    console.log('mount')
+    render(props)
+  },
 
-app.mount('#app')
+  bootstrap() {
+    console.log('bootstrap')
+  },
+  unmount(props) {
+    console.log('unmount', props)
+  }
+})
+
+// 独立运行时
+if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+  render({})
+}
+
+function render(props = {}) {
+  const { container } = props
+  console.log('container', container)
+  const app = createApp(App)
+
+  app.use(router)
+  app.use(createPinia())
+
+  app.mount(container ? container.querySelector('#app') : '#app')
+}
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import qiankun from 'vite-plugin-qiankun'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   return {
-    plugins: [vue()],
+    // 这里的名称要和主应用改造是配置项中的name保持一致
+    plugins: [
+      vue(),
+      qiankun('zs-screen', {
+        useDevMode: true
+      })
+    ],
     server: {
       open: true,
       port: 8888,
@@ -17,7 +24,9 @@ export default defineConfig(({ mode }) => {
           changeOrigin: true,
           rewrite: (path) => path.replace(new RegExp(`^${env.VITE_APP_BASE_API}`), '')
         }
-      }
+      },
+      // 防止开发阶段的assets 静态资源加载问题
+      origin: '//localhost:8888'
     },
     resolve: {
       alias: {
